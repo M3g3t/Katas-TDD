9b3b7a59d8b7f7917935a95c68535432d149dc10
Kevin Lopez <lopezkevin895@gmail.com>
2025-10-20 09:07:47 -0500

    🟥Si_LaEntradaEs1_DebeCalcularUno

--- string-calculator/StringCalculator.Tests/CalculatorTests.cs
+++ string-calculator/StringCalculator.Tests/CalculatorTests.cs
@@ -3,8 +3,12 @@
 public class CalculatorTests
 {
     [Fact]
-    public void Test1()
+    public void Si_LaEntradaEs1_DebeCalcularUno()
     {
+        // Arrange && Act
+        int resultado = Calculadora.Calcular("1");
 
+        // Assert
+        resultado.Should().Be(1);
     }
 }
