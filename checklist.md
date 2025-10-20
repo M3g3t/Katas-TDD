73b7efdb349d0ddc442108cde17068338bfd7d9c
Kevin Lopez <lopezkevin895@gmail.com>
2025-10-20 11:33:20 -0500

    🟦Si_LaEntradaEsMenos2MasDiezMasDos_DebeCalcular10

--- checklist.md
+++ checklist.md
@@ -4,4 +4,4 @@
 - [x] Si digito "-2" el resultado es -2 
 - [x] Si digito "4+3" el resultado es  7 
 - [x] Si digito "5+3" el resultado es 8 
-- [ ] Si digito "-2+10+2" el resultado es 10
+- [x] Si digito "-2+10+2" el resultado es 10
--- string-calculator/StringCalculator/Calculadora.cs
+++ string-calculator/StringCalculator/Calculadora.cs
@@ -5,17 +5,18 @@ public static class Calculadora
     public static int Calcular(string operacion)
     {
         if (operacion.Contains("+"))
-        {
-            int sumatoria = 0;
-            
-            foreach (string digito in operacion.Split("+"))
-                sumatoria += Convert.ToInt32(digito);
-            
-            return sumatoria;
-            
-        }
+            return ResolverSumatoria(operacion);
 
         return Convert.ToInt32(operacion);
+    }
 
+    private static int ResolverSumatoria(string operacion)
+    {
+        int sumatoria = 0;
+            
+        foreach (string digito in operacion.Split("+"))
+            sumatoria += Convert.ToInt32(digito);
+            
+        return sumatoria;
     }
 }
