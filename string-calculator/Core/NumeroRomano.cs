cc44c74ada6616fd0795d9802f4febcd85d864de
Kevin Lopez <lopezkevin895@gmail.com>
2025-10-27 10:31:01 -0500

    🟩 (Numeros romanos) Si_Digito3ElSistema_DebeConvertirAIII

--- string-calculator/Core/NumeroRomano.cs
+++ string-calculator/Core/NumeroRomano.cs
@@ -4,6 +4,12 @@ public static class NumeroRomano
 {
     public static string Convertir(int numero)
     {
-        return  numero == 2 ?  "II" : "I";
+        string r = "";
+
+        for (int i = 1; i <= numero; i++)
+        {
+            r += "I";
+        }
+        return r;
     }
 }
