9c4d62cdc0ec0b6eda7a8974d42e88554b2aa73c
Kevin Lopez <lopezkevin895@gmail.com>
2025-10-27 15:52:03 -0500

    🟩(Numeros romanos) Si_Digito4ElSistema_DebeConvertirAIV

--- string-calculator/Core/NumeroRomano.cs
+++ string-calculator/Core/NumeroRomano.cs
@@ -5,15 +5,16 @@ namespace Core;
 public static class NumeroRomano
 {
 
-    private static Dictionary<short, char> _valores => new()
+    private static Dictionary<short, string> _valores => new()
     {
-        { 1000,'M'},
-        { 500, 'D'},
-        { 100, 'C'},
-        { 50, 'L'},
-        { 10, 'X'},
-        { 5, 'V'},
-        { 1, 'I'}
+        { 1000,"M"},
+        { 500, "D"},
+        { 100, "C"},
+        { 50, "L"},
+        { 10, "X"},
+        { 5, "V"},
+        { 4, "IV"},
+        { 1, "I"}
     };
     public static string Convertir(short numero)
     {
@@ -22,8 +23,8 @@ public static class NumeroRomano
 
     private static string ProcesarNumeroRomano(short numero)
     {
-        if (ProcesarNumeroDirecto(numero) is char letra)
-            return letra.ToString();
+        if (ProcesarNumeroDirecto(numero) is string letra)
+            return letra;
         
         StringBuilder numeroRomano = new StringBuilder();
         
@@ -32,18 +33,18 @@ public static class NumeroRomano
         
         for (int i = 0 ; i < decenas; i++)
         {
-            numeroRomano.Append(_valores[10]);
+            numeroRomano.Append(char.Parse(_valores[10]));
         }
         
         if(unidades > 0)
-            numeroRomano.Append(new string(_valores[1], unidades));
+            numeroRomano.Append(new string(char.Parse(_valores[1]), unidades));
         
         return numeroRomano.ToString();
     }
 
-    private static char? ProcesarNumeroDirecto(short numero)
+    private static string? ProcesarNumeroDirecto(short numero)
     {
-        if (_valores.TryGetValue(numero, out char letra)) return letra;
+        if (_valores.TryGetValue(numero, out string letra)) return letra;
 
         return null;
     }
