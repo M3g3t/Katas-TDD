00a044f86246f8b8a07ab51efa2e0d5a1a2c59e6
Kevin Augusto Lopez Suarez <kevin.lopez@sinco.com.co>
2025-10-28 09:21:17 -0500

    🟩(Numeros romanos) SiDigito63ElSistema_DebeConvertirALXIII

--- string-calculator/Core/NumeroRomano.cs
+++ string-calculator/Core/NumeroRomano.cs
@@ -9,22 +9,22 @@ public static class NumeroRomano
     private static int _numeroAConvertir;
 
     private static int _unidades =>  _numeroAConvertir % 10;
-    private static Dictionary<short, string> _valores => new()
-    {
-        { 1000,"M"},
-        { 900,"CM"},
-        { 500, "D"},
-        { 400, "CD"},
-        { 100, "C"},
-        { 90, "XC"},
-        { 50, "L"},
-        { 40, "XL"},
-        { 10, "X"},
-        { 9, "IX"},
-        { 5, "V"},
-        { 4, "IV"},
-        { 1, "I"}
-    };
+    private static (short numero, string letra)[] _valores =>
+    [
+        ( 1000,"M"),
+        ( 900,"CM"),
+        ( 500, "D"),
+        ( 400, "CD"),
+        ( 100, "C"),
+        ( 90, "XC"),
+        ( 50, "L"),
+        ( 40, "XL"),
+        ( 10, "X"),
+        ( 9, "IX"),
+        ( 5, "V"),
+        ( 4, "IV"),
+        ( 1, "I")
+    ];
     public static string Convertir(short numero)
     {
         _numeroRomanoSb = new();
@@ -32,19 +32,28 @@ public static class NumeroRomano
         
         if (ProcesarNumeroDirecto(numero) is string letra)
             return letra;
-
-        if (_numeroAConvertir is 48) return "XLVIII";
         
-        ProcesarDecenas(numero);
-        ProcesarUnidades();
+            while (numero is not 0)
+            {
+                foreach (var val in _valores)
+                {
+                    if (val.numero <= numero)
+                    {
+                        _numeroRomanoSb.Append(val.letra);
+                        numero -= val.numero;
+                        break;
+                    }
+                }
+            }
         
         return _numeroRomanoSb.ToString();
     }
 
     private static string? ProcesarNumeroDirecto(short numero)
     {
-        _valores.TryGetValue(numero, out string? letra);
-        return letra;
+
+        var valor = _valores.FirstOrDefault(v => v.numero == numero);
+        return valor.letra;
     }
     
     private static void ProcesarDecenas(int numero)
@@ -53,13 +62,12 @@ public static class NumeroRomano
         
         for (int i = 0 ; i < decenas; i++)
         {
-            _numeroRomanoSb!.Append(_valores[10]);
+            _numeroRomanoSb!.Append(_valores.First(v => v.numero == 10));
         }
     }
 
     private static void ProcesarUnidades()
     {
-            
         if (_unidades == 7)
         {
             _numeroRomanoSb!.Append(_valores[5]);
@@ -69,6 +77,6 @@ public static class NumeroRomano
         }
 
         if(_unidades > 0)
-            _numeroRomanoSb!.Append(new string(char.Parse(_valores[1]), _unidades));
+            _numeroRomanoSb!.Append(new string(char.Parse(_valores.First(v => v.numero == 1).letra), _unidades));
     }
 }
