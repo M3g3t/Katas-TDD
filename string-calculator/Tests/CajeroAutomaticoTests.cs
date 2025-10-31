aeff4f8047a2c31fd5243a77a271e5b97c6ecfa6
Kevin Lopez <lopezkevin895@gmail.com>
2025-10-31 14:49:37 -0500

    (Cajero Automatico) 🟥Si_Retiro100ElCajeroAutomatico_DebeMostrarComoSalida1BilleteDeValor100

--- string-calculator/Tests/CajeroAutomaticoTests.cs
+++ string-calculator/Tests/CajeroAutomaticoTests.cs
@@ -21,6 +21,7 @@ public class CajeroAutomaticoTests
     [InlineData(10,"1 billete de valor 10")]
     [InlineData(20,"1 billete de valor 20")]
     [InlineData(50,"1 billete de valor 50")]
+    [InlineData(100,"1 billete de valor 100")]
     public void Si_RetiroValorDelCajeroAutomaticoQueCorrepondaAUnaSolaUnidad_DebeMostrarComoSalidaUnaUnidadConSuValorCorrespondiente(int valorARetirar, string valorEsperado)
     {
         CajeroAutomatico cajeroAutomatico = new();
