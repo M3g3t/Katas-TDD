09062785adeaa53399ce8da4301043c00043511c
KALS895 <lopezkevin895@gmail.com>
2025-11-03 19:39:23 -0500

    (Cajero Automatico)🟥Si_RetiroValor1000DeCajeroAutomatico_Debe_MostrarComoSalida2BilletesDe500

--- string-calculator/Tests/CajeroAutomaticoTests.cs
+++ string-calculator/Tests/CajeroAutomaticoTests.cs
@@ -13,24 +13,26 @@ public class CajeroAutomaticoTests
 
         dineroDisponible.Should().Be(5100);
     }
-    
+
     [Theory]
-    [InlineData(1,"1 moneda de valor 1")]
-    [InlineData(2,"1 moneda de valor 2")]
-    [InlineData(5,"1 billete de valor 5")]
-    [InlineData(10,"1 billete de valor 10")]
-    [InlineData(20,"1 billete de valor 20")]
-    [InlineData(50,"1 billete de valor 50")]
-    [InlineData(100,"1 billete de valor 100")]
-    [InlineData(200,"1 billete de valor 200")]
-    [InlineData(500,"1 billete de valor 500")]
-    public void Si_RetiroValorDelCajeroAutomaticoQueCorrepondaAUnaSolaUnidad_DebeMostrarComoSalidaUnaUnidadConSuValorCorrespondiente(int valorARetirar, string valorEsperado)
+    [InlineData(1, "1 moneda de valor 1")]
+    [InlineData(2, "1 moneda de valor 2")]
+    [InlineData(5, "1 billete de valor 5")]
+    [InlineData(10, "1 billete de valor 10")]
+    [InlineData(20, "1 billete de valor 20")]
+    [InlineData(50, "1 billete de valor 50")]
+    [InlineData(100, "1 billete de valor 100")]
+    [InlineData(200, "1 billete de valor 200")]
+    [InlineData(500, "1 billete de valor 500")]
+    public void
+        Si_RetiroValorDelCajeroAutomaticoQueCorrepondaAUnaSolaUnidad_DebeMostrarComoSalidaUnaUnidadConSuValorCorrespondiente(
+            int valorARetirar, string valorEsperado)
     {
         CajeroAutomatico cajeroAutomatico = new();
 
         var salida = cajeroAutomatico.Retirar(valorARetirar);
-   
-        salida.Should().Be(valorEsperado);
+
+        salida.Should().BeEquivalentTo(new List<string> { valorEsperado });
     }
 
     [Fact]
@@ -50,7 +52,17 @@ public class CajeroAutomaticoTests
 
         var salida = cajeroAutomatico.Retirar(700);
 
-        salida.Should().BeEquivalentTo(new List<string>(){"1 billete de valor 500","1 billete de valor 200"});
+        salida.Should().BeEquivalentTo(new List<string> { "1 billete de valor 500", "1 billete de valor 200" });
+    }
 
+    [Fact]
+    public void Si_RetiroValor1000DeCajeroAutomatico_Debe_MostrarComoSalida2BilletesDe500()
+    {
+        CajeroAutomatico cajeroAutomatico = new();
+
+        var salida = cajeroAutomatico.Retirar(1000);
+
+        salida.Should().HaveCount(1);
+        salida.Should().BeEquivalentTo(new List<string> { "2 billetes de valor 500" });
     }
 }
