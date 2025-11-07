88e8b3a9f945a12d38462b707d134e334d3b6679
KALS895 <lopezkevin895@gmail.com>
2025-11-07 10:45:14 -0500

    (Caja Registradora) 🟩Si_RegistroProductoConPrecio10_ElValorAPagar_Debe_Ser10

--- string-calculator/Core/CajaRegistradora.cs
+++ string-calculator/Core/CajaRegistradora.cs
@@ -3,4 +3,9 @@ namespace Core;
 public class CajaRegistradora
 {
     public decimal ValorRegistrado { get; private set; } = 0;
+
+    public void RegistrarProducto(int i)
+    {
+        ValorRegistrado = 10;
+    }
 }
