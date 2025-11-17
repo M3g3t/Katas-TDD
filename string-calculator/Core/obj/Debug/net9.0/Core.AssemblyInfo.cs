bd41d1ce31c0673c42168817c9fd40b78cb0f83c
KALS895 <lopezkevin895@gmail.com>
2025-11-17 18:06:05 -0500

    WordWrap

--- string-calculator/Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
+++ string-calculator/Core/obj/Debug/net9.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c697ce6f21d0987a7f89a7569d5f40fbf9d7d2db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3d998b11ddfdd76fe4ae3b524d00e724f13833d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
